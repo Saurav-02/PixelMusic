189239d31ebe9e5b39b984db31ec111d6778028d
saurav-home <bookjangle65@gmail.com>
2026-07-24 08:48:48 +0530

    Update URL in AboutScreen to use GitHub Pages

--- app/src/main/java/com/unshoo/pixelmusic/presentation/screens/AboutScreen.kt
+++ app/src/main/java/com/unshoo/pixelmusic/presentation/screens/AboutScreen.kt
@@ -437,7 +437,7 @@ private fun SocialLinksRow() {
                 .weight(1f)
                 .clickable {
                     // 2. Force a native Android intent here as well
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://sauravbr.github.in/PixelMusic"))
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://sauravbr.github.io/PixelMusic"))
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                     try {
                         context.startActivity(intent)
