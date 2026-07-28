bc8d7c2e8d7bd5b3a2f5890abd6ff78d82fe9927
SAURAV-DBS <atappu805@gmail.com>
2026-07-28 15:30:56 +0530

    Update SettingsCategoryScreen.kt

--- app/src/main/java/com/unshoo/pixelmusic/presentation/screens/SettingsCategoryScreen.kt
+++ app/src/main/java/com/unshoo/pixelmusic/presentation/screens/SettingsCategoryScreen.kt
@@ -82,6 +82,7 @@ import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material.icons.rounded.ChevronRight
 import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Download
 import androidx.compose.material.icons.rounded.ExpandMore
 import androidx.compose.material.icons.rounded.MusicNote
 import androidx.compose.material.icons.rounded.Restore
