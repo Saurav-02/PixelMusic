bd0d617405a6f859b02083d90024dd4b4678c877
atappu805 <atappu805@gmail.com>
2026-09-16 05:54:59 +0530

    Update MainActivity.kt

--- app/src/main/java/com/unshoo/pixelmusic/MainActivity.kt
+++ app/src/main/java/com/unshoo/pixelmusic/MainActivity.kt
@@ -1256,6 +1256,7 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                     }
+                    }
                 }
             }
         }
